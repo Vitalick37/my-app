087256c4d5a6866d8c6a49cd8ddfe9699e797c9d
Vitalick37 <vit198837@mail.ru>
2021-01-19 20:46:22 +0300

    26lesson

--- src/components/Content/MyPosts/MyPosts.jsx
+++ src/components/Content/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        {id: 1, post: 'how are you?', like: 3},
+        {id: 2, post: 'i`m fine', like: 7},
+    ]
+
+    let postElement = postData.map(el => <Post message={el.post} like={el.like} />)
+
     return (
         <div className={classes.my_posts}>
             <p>my posts</p>
@@ -10,8 +18,10 @@ const MyPosts = () => {
                 <textarea name="" id="" cols="30" rows="10"></textarea>
                 <button>Add post</button>
             </div>
-            <Post message='how are you?' like='3' />
-            <Post message='i`m fine' like='7' />
+            {/* <Post message={postData[0].post} like={postData[0].like} />
+            <Post message={postData[1].post} like={postData[1].like} /> */}
+
+            {postElement}
         </div>
     );
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,26 +16,52 @@ const MessageItem = (props) => {
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        { id: 1, name: 'Dima' },
+        { id: 2, name: 'Mihael' },
+        { id: 3, name: 'Andre' },
+        { id: 4, name: 'Anna' },
+        { id: 5, name: 'Nikolya' },
+        { id: 6, name: 'Margo' },
+    ]
+
+    let dialogsElement = dialogsData.map(element => <DialogItem name={element.name} id={element.id} />);
+
+    let messageData = [
+        { id: 1, message: 'hi' },
+        { id: 2, message: 'how are you?' },
+        { id: 3, message: 'i`m fine' },
+    ]
+
+    let messageElement = messageData.map(el => <MessageItem messageItem={el.message} />);
+
+
+
     return (
         <BrowserRouter>
             <div className={s.wrapper}>
                 <div className={s.name}>
                     <ul className={s.name_list}>
                         {/* <Route path='/numberUser' component={DialogItem} /> */}
-                        <DialogItem name='Dima' id='1' />
+                        {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
                         <DialogItem name='Mihael' id='2' />
                         <DialogItem name='Andre' id='3' />
                         <DialogItem name='Anna' id='4' />
                         <DialogItem name='Nikolya' id='5' />
-                        <DialogItem name='Margo' id='6' />
+                        <DialogItem name='Margo' id='6' /> */}
+
+                        { dialogsElement }
                     </ul>
                 </div>
                 <div className={s.message}>
                     <ul className={s.message_list}>
                         {/* <Route path='/numberMessage' component={MessageItem} /> */}
-                        <MessageItem messageItem='hi'/>
-                        <MessageItem messageItem='how are you?'/>
-                        <MessageItem messageItem='i`m fine'/>
+                        {/* <MessageItem messageItem={messageData[0].message} />
+                        <MessageItem messageItem='how are you?' />
+                        <MessageItem messageItem='i`m fine' /> */}
+
+                        {messageElement}
                     </ul>
                 </div>
             </div>
